25e44e6d25ac402798168b419136151746c02462
Simon Mika <simon@mika.se>
2019-11-12 12:47:37 +0100

    Added support for arrays.

--- index.ts
+++ index.ts
@@ -2,11 +2,13 @@ import { and } from "./And"
 import { any } from "./Any"
 import { Criteria } from "./Criteria"
 import { endsWith } from "./EndsWith"
+import { every } from "./Every"
 import { includes } from "./Includes"
 import { filter, is } from "./Is"
 import { match } from "./Match"
 import { not } from "./Not"
 import { or } from "./Or"
+import { some } from "./Some"
 import { startsWith } from "./StartsWith"
 
 export {
@@ -14,11 +16,13 @@ export {
 	any,
 	and,
 	endsWith,
+	every,
 	filter,
 	includes,
 	is,
 	match,
 	not,
 	or,
+	some,
 	startsWith,
 }
