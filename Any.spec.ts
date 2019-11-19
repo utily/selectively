c64edc8ab5451b0a4ab80ce7700237ce18ca6860
Simon Mika <simon@mika.se>
2019-11-19 14:53:46 +0100

    Fixed issue with null properties.

--- Any.spec.ts
+++ Any.spec.ts
@@ -4,4 +4,5 @@ describe("any", () => {
 	it("simple", () => expect(selectively.is(selectively.any("test"), { id: "axb", class: "test" })).toBeTruthy())
 	it("deep", () => expect(selectively.is(selectively.any("test"), { id: "axb", class: { name: "test" } })).toBeTruthy())
 	it("not", () => expect(selectively.is(selectively.any("test"), { id: "axb", class: { name: "test2" } })).toBeFalsy())
+	it("null", () => expect(selectively.is(selectively.any("test"), { id: null, class: { name: "test" } })).toBeTruthy())
 })
