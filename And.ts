668313d8ca8dcac3caadf2cbd6e4080da4b2136c
Simon Mika <simon@mika.se>
2019-11-26 12:53:25 +0100

    Cleanup.

--- And.ts
+++ And.ts
@@ -3,14 +3,14 @@ import { Criteria } from "./Criteria"
 
 export class And extends Rule {
 	readonly precedence = 40
-	constructor(readonly criterias: Rule[]) {
+	constructor(readonly rules: Rule[]) {
 		super()
 	}
 	is(value: any): boolean {
-		return this.criterias.every(c => c.is(value))
+		return this.rules.every(c => c.is(value))
 	}
 	toString() {
-		return this.criterias.map(c => c.stringify(this.precedence)).join(" ")
+		return this.rules.map(c => c.stringify(this.precedence)).join(" ")
 	}
 }
 export function and(...criterias: Criteria[]): And {
