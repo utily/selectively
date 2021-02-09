ef05a72fa860daca520b9fd99b90ccb16e7c3670
Robin Nedergård <robin.nedergard@payfunc.com>
2021-02-09 10:52:36 +0100

    fixed parsing Exists

--- parse/Exists.ts
+++ parse/Exists.ts
@@ -2,6 +2,6 @@ import { Exists } from "../Exists"
 import { add } from "./parse"
 
 add(source => {
-	const fetched = source.fetchIf("exists(", "any", ")")
-	return fetched && new Exists(fetched[1].value)
+	const fetched = source.fetchIf("exists", "(", "any", ")")
+	return fetched && new Exists(fetched[2].value)
 })
