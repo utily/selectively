d219537ae267ce67f2ca56a28746f38bd08ed86a
marcushanses <79835961+marcushanses@users.noreply.github.com>
2021-03-16 15:48:09 +0100

    Rewrote autocomplete for properties. (#38)

--- Type/Object.ts
+++ Type/Object.ts
@@ -12,36 +12,17 @@ export class TObject extends SType {
 	}
 
 	complete(tokens: Token[]): Completion[] {
-		let result: Completion[]
-		switch (tokens.length) {
-			case 0:
-				result = []
-				break
-			case 1:
-				if (tokens[0].value == ".")
-					result = Completion.prepend(".", this.completions)
-				else
-					result = []
-				break
-			default:
-				if (tokens[0].value != ".")
-					result = []
-				else {
-					const propertyType = this.properties[tokens[1].value]
-					if (propertyType)
-						result = Completion.prepend("." + tokens[1].value, propertyType.complete(tokens.slice(2)))
-					else if (tokens.length == 2)
-						result = Completion.prepend(
-							".",
-							this.completions.filter(c => c.value.startsWith(tokens[1].value))
-						)
-					else
-						result = []
-					break
-				}
-		}
-		result = [
-			...result,
+		return [
+			...(tokens.length == 0 || tokens[0].value != "."
+				? []
+				: tokens.length == 1
+				? Completion.prepend(".", this.completions)
+				: this.properties[tokens[1].value]
+				? Completion.prepend("." + tokens[1].value, this.properties[tokens[1].value].complete(tokens.slice(2)))
+				: Completion.prepend(
+						".",
+						this.completions.filter(c => c.value.startsWith(tokens[1].value))
+				  )),
 			...TObject.completor
 				.map(p => p(tokens, this))
 				.reduce<Completion[]>((result, element) => result.concat(element), [])
@@ -51,7 +32,6 @@ export class TObject extends SType {
 					[]
 				),
 		]
-		return result
 	}
 
 	private static readonly completor: Completor<TObject>[] = []
