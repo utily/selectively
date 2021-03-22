c6cc5f85e6297007dd53d5ec3107430a13083c77
Marcus Hanses <marcus.hanses@intergiro.com>
2021-03-22 17:08:18 +0100

    Added evaluation function.
    Removed comment.
    
    Removed comment.
    
    Added parenthesis and moved the evaluate function.

--- BinaryOperator.ts
+++ BinaryOperator.ts
@@ -1,8 +1,24 @@
-import { Expression } from "./Expression"
-
 export abstract class BinaryOperator {
 	readonly precedence: number
+	abstract evaluate(left: number, right: number): number
 	readonly symbol: string
-	readonly left: Expression
-	readonly right: Expression
+	readonly left: BinaryOperator | number
+	readonly right: BinaryOperator | number
+	operate(): number {
+		return this.evaluate(
+			typeof this.left == "number" ? this.left : this.left.operate(),
+			typeof this.right == "number" ? this.right : this.right.operate()
+		)
+	}
+	toString(): string {
+		return (
+			(this.precedence > (typeof this.left == "number" ? this.precedence : this.left.precedence) ? "(" : "") +
+			this.left.toString() +
+			(this.precedence > (typeof this.left == "number" ? this.precedence : this.left.precedence) ? ")" : "") +
+			this.symbol +
+			(this.precedence > (typeof this.right == "number" ? this.precedence : this.right.precedence) ? "(" : "") +
+			this.right.toString() +
+			(this.precedence > (typeof this.right == "number" ? this.precedence : this.right.precedence) ? ")" : "")
+		)
+	}
 }
--- Multiplication.ts
+++ Multiplication.ts
@@ -1,17 +1,14 @@
 import { BinaryOperator } from "./BinaryOperator"
-import { Expression } from "./Expression"
 
 export class Multiplication extends BinaryOperator {
 	readonly precedence = 15
 	readonly symbol = "*"
-	constructor(readonly left: Expression, readonly right: Expression) {
+
+	constructor(readonly left: BinaryOperator | number, readonly right: BinaryOperator | number) {
 		super()
 	}
-	toString(): string {
-		return this.left + "*" + this.right
-	}
-}
 
-export function multiply(left: Expression, right: Expression): number | undefined {
-	return typeof left == "number" && typeof right == "number" ? left + right : undefined
+	evaluate(left: number, right: number): number {
+		return left * right
+	}
 }
