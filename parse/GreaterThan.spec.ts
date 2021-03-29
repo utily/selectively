6e1958ae6bebc2990ea8e9e8a08860d7c78c5b80
Marcus Hanses <marcus.hanses@intergiro.com>
2021-03-29 14:36:57 +0200

    Added parsing for binary operators.
    parent bd6a49f525c4fc6b00846fe0178a6aa52d1f316a
    author Marcus Hanses <marcus.hanses@intergiro.com> 1617021417 +0200
    committer Marcus Hanses <marcus.hanses@intergiro.com> 1617275470 +0200
    
    Added algebraic expressions lesser than and greater than parser.
    
    parent bd6a49f525c4fc6b00846fe0178a6aa52d1f316a
    author Marcus Hanses <marcus.hanses@intergiro.com> 1617021417 +0200
    committer Marcus Hanses <marcus.hanses@intergiro.com> 1617275470 +0200
    
    Added algebraic expressions lesser than and greater than parser.
    
    Fixed comments.
    
    Fixed comments.

--- parse/GreaterThan.spec.ts
+++ parse/GreaterThan.spec.ts
@@ -2,7 +2,9 @@ import * as selectively from "../index"
 
 describe("parse.GreaterThan", () => {
 	it("test rules related comparisons", () => {
-		expect(selectively.parse("amount>5")).toEqual(new selectively.Property("amount", new selectively.GreaterThan(5)))
+		expect(selectively.parse("amount>5")).toEqual(
+			new selectively.Property("amount", new selectively.GreaterThan(new selectively.Value(5)))
+		)
 		expect(selectively.parse("amount>5").is({ amount: 6 })).toBeTruthy()
 		expect(selectively.parse("amount>5").is({ amount: 4 })).toBeFalsy()
 		expect(selectively.parse("verification.amount>5").is({ verification: { amount: 4 } })).toBeFalsy()
--- parse/LesserThan.ts
+++ parse/LesserThan.ts
@@ -1,7 +1,6 @@
 import { LesserThan } from "../LesserThan"
-import { add } from "./parse"
+import { add, parseExpression } from "./parse"
 
 add(source => {
-	const fetched = source.fetchIf("<", "any")
-	return fetched && new LesserThan(isNaN(+fetched[1].value) ? fetched[1].value : +fetched[1].value)
+	return source.fetchIf("<") && new LesserThan(parseExpression(source))
 })
