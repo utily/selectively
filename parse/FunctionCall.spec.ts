b196f758d153b28b328eafaf7a479baf7f534725
marcushanses <79835961+marcushanses@users.noreply.github.com>
2022-02-15 23:01:11 +0100

    Fixed bug with functions without arguments. (#108)
    * Fixed bug with functions without arguments.
    Removed infinite loop when functions didn't have a definition.
    Added tests.
    
    * Requested changes.

--- parse/FunctionCall.spec.ts
+++ parse/FunctionCall.spec.ts
@@ -1,6 +1,15 @@
 import * as selectively from "../index"
 
 describe("parse.FunctionCall", () => {
+	it("amount()", () => {
+		expect(selectively.parse("amount()")).toEqual({
+			argument: [""],
+			class: "FunctionCall",
+			definition: undefined,
+			identifier: "amount",
+			precedence: 85,
+		})
+	})
 	it("amount(5)", () => {
 		expect(selectively.parse("amount(5)")).toEqual({
 			argument: ["5"],
@@ -13,6 +22,15 @@ describe("parse.FunctionCall", () => {
 	it("amount(5, 6)", () => {
 		expect(selectively.parse("amount(5, 6)")).toEqual({
 			argument: ["5", "6"],
+			class: "FunctionCall",
+			definition: undefined,
+			identifier: "amount",
+			precedence: 85,
+		})
+	})
+	it("amount(2022-02-22)", () => {
+		expect(selectively.parse("amount(2022-02-22)")).toEqual({
+			argument: ["2022-02-22"],
 			class: "FunctionCall",
 			definition: undefined,
 			identifier: "amount",
--- parse/FunctionCall.ts
+++ parse/FunctionCall.ts
@@ -3,12 +3,15 @@ import { add } from "./parse"
 
 add(source => {
 	const peek = source.peek(3)?.value
-	const fetched = [")", ",", "("].find(s => s == peek) ? source.fetchIf("identifier", "(") : undefined
+	const fetched =
+		[")", ",", "("].find(s => s == peek) || source.peek(2)?.value == ")" ? source.fetchIf("identifier", "(") : undefined
 	let parameters: string[] | undefined
 	if (fetched && source.peek()) {
-		parameters = [source.fetch()?.value ?? ""]
+		// eslint-disable-next-line no-useless-escape
+		parameters = [source.fetchIf(/[A-Za-z0-9_\.\-]+/)?.value ?? ""]
 		while (source.fetchIf(","))
-			parameters.push(source.fetchIf("any")?.value ?? "")
+			// eslint-disable-next-line no-useless-escape
+			parameters.push(source.fetchIf(/[A-Za-z0-9_\.\-]+/)?.value ?? "")
 		if (!source.fetchIf(")"))
 			source.raise("Missing end of parenthesis.")
 	}
