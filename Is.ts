02daa18de2bd97e2e4455b40ef74e0df6eb78204
Simon Mika <simon@mika.se>
2023-04-06 19:04:18 +0200

    Fika testing.

--- Is.ts
+++ Is.ts
@@ -14,8 +14,11 @@ export class Is extends Rule {
 	is(value: any, object?: any): boolean {
 		return Array.isArray(value) && isCriteria(this.value)
 			? some(this.value, value)
-			: (typeof value == "boolean" ? value.toString() : isNaN(+value) ? value : +value) ==
-					(typeof this.value == "object" ? this.value.evaluate(object) : this.value)
+			: (!isNaN(+value)
+					? +value
+					: typeof value == "boolean" && typeof this.value == "string"
+					? value.toString()
+					: value) == (typeof this.value == "object" ? this.value.evaluate(object) : this.value)
 	}
 	toString(): string {
 		return this.value.toString()
--- index.spec.ts
+++ index.spec.ts
@@ -7,4 +7,23 @@ describe("Filter", () => {
 		const object = [{ authorization: { number: "filterThis" } }, { authorization: { number: "doNotFilterThis" } }]
 		expect(parsed.filter(object)).toEqual([{ authorization: { number: "doNotFilterThis" } }])
 	})
+	it("fika", () => {
+		const data = [
+			{
+				seat: { exit: true, wide: true, window: false, toilet: false, position: { deck: 0, row: 22, column: "C" } },
+				passenger: { age: "adult" },
+			},
+			{
+				seat: { exit: true, wide: true, window: false, toilet: false, position: { deck: 0, row: 25, column: "B" } },
+				passenger: { age: "child" },
+			},
+			{
+				seat: { exit: false, wide: false, window: true, toilet: false, position: { deck: 0, row: 26, column: "A" } },
+				passenger: { age: "child" },
+			},
+		]
+		const rule = selectively.parse("passenger.age:child seat.exit:true") as selectively.And
+		console.log(JSON.stringify(rule))
+		expect(rule.filter(data)).toMatchSnapshot()
+	})
 })
